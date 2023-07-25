f4877c636e4b02a830d252499ab51315e16c3f56
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-07-25 13:01:35 -0500

    homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return(
+        <>
+        <div className="d-flex justify-content-center me-5">
+        <div className="homepage-banner neon-text">
+        Trivia Trainer</div>  
+        </div>
+        <div className="homepage-container py-5">        
+        </div>   
+        </>   
+    );
+}
